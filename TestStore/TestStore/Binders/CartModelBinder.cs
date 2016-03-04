a0ab27de566b95ed59c3430458b275a324656adb
olegik7711 <olegik7711@bk.ru>
2016-03-04 20:33:09 +0300

    3

---
+++ TestStore/TestStore/Binders/CartModelBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TestStore.Models;
+using System.Web.Mvc;
+namespace TestStore.Binders
+{
+    public class CartModelBinder:IModelBinder
+    {
+        private const string sessionKey = "CartInfo";
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            CartInfo cart = (CartInfo)controllerContext.HttpContext.Session[sessionKey];
+            if(cart==null)
+            {
+                cart = new CartInfo();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+            return cart;
+
+
+        }
+    }
+}
